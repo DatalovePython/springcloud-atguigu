899a030dd7d111aff408c40a6ab19a5b28273d12
luzhongchuan <wyhyy20161121@163.com>
2020-12-13 22:41:31 +0800

    Eureka 集群环境构建

--- cloud-eureka-server7001/src/main/java/com/newland/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/newland/springcloud/EurekaMain7001.java
@@ -3,18 +3,18 @@ package com.newland.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author luzc
- * @date 2020/12/5 12:16
+ * @date 2020/12/12 16:14
  * @desc
  */
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
-
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
