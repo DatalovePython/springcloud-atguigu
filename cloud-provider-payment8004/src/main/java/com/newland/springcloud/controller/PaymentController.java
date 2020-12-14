a738e664f6d2c503aae21c82fa77473673eb5b3e
luzhongchuan <wyhyy20161121@163.com>
2020-12-14 15:30:51 +0800

    支付服务注册进zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/newland/springcloud/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package com.newland.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author luzc
+ * @date 2020/12/14 15:04
+ * @desc
+ */
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk() {
+        return "springcloud with zookeeper:" + serverPort + "\t"
+                + UUID.randomUUID().toString();
+    }
+}
+
+
