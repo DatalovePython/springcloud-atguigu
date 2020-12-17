8e141ddccb58b8ff566db1174e41949097cc67d8
luzhongchuan <wyhyy20161121@163.com>
2020-12-17 18:24:18 +0800

    Gateway 工程搭建

---
+++ cloud-gateway-gateway9527/src/main/java/com/newaland/springcloud/GatewayMain9527.java
@@ -0,0 +1,19 @@
+package com.newaland.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author luzc
+ * @date 2020/12/17 16:55
+ * @desc
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
