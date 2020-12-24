307dc88bb3712e394bbd0017861c3a1ff6ac6eeb
luzhongchuan <wyhyy20161121@163.com>
2020-12-24 15:14:47 +0800

    Gateway 常用的Predicate

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author luzc
+ * @date 2020/12/24 14:51
+ * @desc
+ */
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
