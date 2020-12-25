030a18c807fb6c71532c36e695e57139adac32ee
luzhongchuan <wyhyy20161121@163.com>
2020-12-25 14:58:11 +0800

    Config 客户端配置与测试

---
+++ cloud-config-client-3355/src/main/java/com/newland/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.newland.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author luzc
+ * @date 2020/12/25 14:25
+ * @desc
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
