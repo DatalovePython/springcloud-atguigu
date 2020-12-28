d88036a1be95647416bae057381502b3f5088553
luzhongchuan <wyhyy20161121@163.com>
2020-12-28 17:05:14 +0800

    bus 消息总线-动态刷新全局广播配置实现

---
+++ cloud-config-client-3366/src/main/java/com/newland/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,18 @@
+package com.newland.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author luzc
+ * @date 2020/12/28 16:21
+ * @desc
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ cloud-config-client-3366/src/main/java/com/newland/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,27 @@
+package com.newland.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luzc
+ * @date 2020/12/25 14:27
+ * @desc
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return "serverPort: " + serverPort + "\t\n\n configInfo" + configInfo;
+    }
+}
