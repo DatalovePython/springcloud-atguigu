efda666e8f8691d78dc1fb1b9ab348872e931c62
wyhyy20161121@163.com <Photon>
2020-12-30 16:39:39 +0800

    Nacos 服务提供者注册

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/newland/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.newland.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author luzc
+ * @date 2020/12/30 16:24
+ * @desc
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "Hello Nacos Discovery: " + serverPort + "\t id: " + id;
+    }
+}
