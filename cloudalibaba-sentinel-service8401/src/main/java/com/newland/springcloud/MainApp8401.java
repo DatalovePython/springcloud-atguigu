9753ee2a2707e9a626896b0e3926fd0f6b493f4b
luzhongchuan <wyhyy20161121@163.com>
2021-01-06 15:06:15 +0800

    Sentinel 初始化监控

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/newland/springcloud/MainApp8401.java
@@ -0,0 +1,19 @@
+package com.newland.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author luzc
+ * @date 2021/1/6 15:00
+ * @desc
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MainApp8401 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp8401.class, args);
+    }
+}
